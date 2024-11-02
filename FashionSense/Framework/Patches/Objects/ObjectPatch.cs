4c949efc37f42b7e0014c3e307ed3c436e40e86d
Floogen <31755155+Floogen@users.noreply.github.com>
2024-11-01 23:59:25 -0500

    Minor code cleanup

--- FashionSense/Framework/Patches/Objects/ObjectPatch.cs
+++ FashionSense/Framework/Patches/Objects/ObjectPatch.cs
@@ -4,7 +4,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewValley;
-using StardewValley.Objects;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
